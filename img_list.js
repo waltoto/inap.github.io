9694af6f302304d2f2891fccb67ddfda92dc475c
waltoto <100846528+waltoto@users.noreply.github.com>
2024-12-17 22:29:48 +0100

    Update img_list.js

--- img_list.js
+++ img_list.js
@@ -1,19 +1,19 @@
-<script>
-  function changeImage() {
-    // Get the value of the dropdown
-    var choice = document.getElementById("choice").value;
 
-    // Check if no option is selected
-    if (choice === "") {
-      alert("No Image Selected!");
-      return;
-    }
+function changeImage() {
+  // Get the value of the dropdown
+  var choice = document.getElementById("choice").value;
 
-    // Path to the image based on choice
-    var imagePath = "assets/img/" + choice + ".png";
-
-    // Update the result div with the selected image
-    var result = document.getElementById("result");
-    result.innerHTML = "<img src='" + imagePath + "' style='max-width: 100%; height: 100%;' alt='Selected Image'>";
+  // Check if no option is selected
+  if (choice === "") {
+    alert("No Image Selected!");
+    return;
   }
-</script>
+
+  // Path to the image based on choice
+  var imagePath = "assets/img/" + choice + ".png";
+
+  // Update the result div with the selected image
+  var result = document.getElementById("result");
+  result.innerHTML = "<img src='" + imagePath + "' style='max-width: 100%; height: 100%;' alt='Selected Image'>";
+}
+
