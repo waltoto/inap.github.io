32ce5b5004fe63b430688206c61bd9c523f702bc
waltoto <100846528+waltoto@users.noreply.github.com>
2024-12-17 19:40:41 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 const images = [
-    'assets/img/distribution-actor-director-age-1-film.png',
     'assets/img/distribution-actor-director-age-movie.png',
     'assets/img/distribution-actor-gender.png',
+    'assets/img/distribution-actor-director-age-1-film.png'
 ];
 
 document.addEventListener('DOMContentLoaded', () => {
