e743f334e04225a265fab7095e7d4a79f7d5ce79
waltoto <100846528+waltoto@users.noreply.github.com>
2024-12-18 14:20:08 +0100

    Update html_list.js

--- html_list.js
+++ html_list.js
@@ -1,18 +1,17 @@
-
 function changeImage() {
   // Get the value of the dropdown
   var choice = document.getElementById("choice").value;
 
   // Check if no option is selected
   if (choice === "") {
-    alert("No Image Selected!");
+    alert("No Content Selected!");
     return;
   }
 
-  // Path to the image based on choice
-  var imagePath = "assets/img/" + choice + ".htlm";
+  // Path to the HTML file based on choice
+  var htmlPath = "assets/html/" + choice + ".html"; // Fixed file extension
 
-  // Update the result div with the selected image
+  // Update the result div with the selected HTML content in an iframe
   var result = document.getElementById("result");
   result.innerHTML = "<iframe src='" + htmlPath + "' style='width: 100%; height: 100%; border: none;'></iframe>";
 }
