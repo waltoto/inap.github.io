f334f96fcca5349b1a4bcfcd40c4b51b98136571
Thibault <100865441+schiesser@users.noreply.github.com>
2024-12-18 17:01:26 +0100

    Update img_list.js

--- img_list.js
+++ img_list.js
@@ -17,3 +17,20 @@ function changeImage() {
   result.innerHTML = "<img src='" + imagePath + "' style='max-width: 100%; height: 100%;' alt='Selected Image'>";
 }
 
+function changeImageHTML() {
+  // Get the value of the dropdown
+  var choice = document.getElementById("choice").value;
+
+  // Check if no option is selected
+  if (choice === "") {
+    alert("No Image Selected!");
+    return;
+  }
+
+  // Path to the image based on choice
+  var imagePath = "assets/img/" + choice + ".html";
+
+  // Update the result div with the selected image
+  var result = document.getElementById("result");
+  result.innerHTML = "<img src='" + imagePath + "' style='max-width: 100%; height: 100%;' alt='Selected Image'>";
+}
